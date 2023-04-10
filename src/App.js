64955e35ad14570bf1e9109781cf4be876ca1fa2
Pridhviraj511 <101736606+Pridhviraj511@users.noreply.github.com>
2023-04-10 15:39:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pridhvi's Project
+          Pridhvi's  New Project
         </p>
         <a
           className="App-link"
